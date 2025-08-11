649b604b7ae5b5131b065aeb2308198d18fa5c3f
Subtracting <subtracting@gmail.com>
2025-08-11 23:12:17 +0200

    moved to main

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,15 @@
+import ArtGallery from "@/components/ArtGallery"
+import Hero from "@/components/Hero"
+import Misc from "@/components/Misc"
+import Projects from "@/components/Projects"
+
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-    </div>
-  );
+    <div>
+      <Hero></Hero>
+      <ArtGallery></ArtGallery>
+      <Projects></Projects>
+      <Misc></Misc>
+   </div>
+  )
 }
