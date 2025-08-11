230d4a90b846ff1db74ab0c2e5086bc6384b70bf
Subtracting <subtracting@gmail.com>
2025-08-11 22:45:58 +0200

    responsive hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,13 +2,16 @@ import Image from 'next/image'
 
 export default function Hero() {
   return (
-    <div id="home" className="p-4 flex flex-row gap-8">
+    <div
+        id="home"
+        className="p-4 flex flex-col md:flex-row gap-8 items-center md:items-start"
+    >
       <div className="rounded-md p-8 flex-1 content-evenly text-2xl">
           <h2 className="text-3xl font-black tracking-tighter mb-6 opacity-30">
             I AM
           </h2>
 
-          <h1 className="text-6xl font-black tracking-tighter mb-6 opacity-90">
+          <h1 className="text-5xl md:text-6xl font-black tracking-tighter mb-6 opacity-90">
            SUBTRACTING
           </h1>
         <p className="text-1xl tracking-wide opacity-60">
@@ -16,13 +19,15 @@ export default function Hero() {
           a passion for problem-solving, puzzles and patterns. coding and art allow me to express and challenge myself.
         </p>
       </div>
-      <Image
-        src="/art/portrait2.png"
-        alt="my_portrait"
-        width={350}
-        height={400}
-        className="rounded-lg object-cover flex-none"
-      />
+      <div className="flex-none w-full max-w-sm min-w-[150px]">
+        <Image
+          src="/art/portrait2.png"
+          alt="my_portrait"
+          width={350}
+          height={400}
+          className="rounded-lg object-cover w-full h-auto"
+        />
+      </div>
     </div>
   )
 }
