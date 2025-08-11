0cfba47aa248d0ffb6ffb3ac4d220fccf830e271
Subtracting <subtracting@gmail.com>
2025-08-11 22:51:29 +0200

    responsive hero styling

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,14 +4,14 @@ export default function Hero() {
   return (
     <div
         id="home"
-        className="p-4 flex flex-col md:flex-row gap-8 items-center md:items-start"
+        className="p-4 flex flex-col md:flex-row gap-8"
     >
       <div className="rounded-md p-8 flex-1 content-evenly text-2xl">
           <h2 className="text-3xl font-black tracking-tighter mb-6 opacity-30">
             I AM
           </h2>
 
-          <h1 className="text-5xl md:text-6xl font-black tracking-tighter mb-6 opacity-90">
+          <h1 className="text-4xl md:text-6xl font-black tracking-tighter mb-6 opacity-90">
            SUBTRACTING
           </h1>
         <p className="text-1xl tracking-wide opacity-60">
@@ -25,7 +25,7 @@ export default function Hero() {
           alt="my_portrait"
           width={350}
           height={400}
-          className="rounded-lg object-cover w-full h-auto"
+          className="rounded-lg object-cover h-auto"
         />
       </div>
     </div>
